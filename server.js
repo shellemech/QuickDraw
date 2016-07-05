887923f4c80a5280c3c016b141d9888530ffaf05
Shellemech <shellemech@gmail.com>
2016-07-05 10:18:20 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,97 @@
+﻿//Web server
+var web = require("./server/web.js");
+web.start(8443);
+
+//Main logic
+var socket;
+var rooms = [];
+var roomIds = {};
+
+// get entire room including data points and map
+function getRoom(roomId)
+{	
+    if (!(roomId in roomIds))
+    {
+        roomIds[roomId] = rooms.length;
+        rooms.push([]);
+    } 
+    return rooms[roomIds[roomId]];
+}
+
+//send room back to client
+function sendRoom(roomId)
+{ 
+    web.io.emit("servermessage", {
+        "room": roomId,
+        "command": "send_room",
+        "data": getRoom(roomId)
+    });
+}
+
+//add data points (if not the pin type) to the room array
+function draw(msg)
+{
+    var room = getRoom(msg["room"]);
+    if (msg["data"]['shape'] != "pin") room.push(msg["data"]);
+    web.io.emit("servermessage", {
+        "room": msg["room"],
+        "command": "draw",
+        "data": msg["data"]
+    });
+}
+
+//add map to room array
+function map(msg)
+{
+    var room = getRoom(msg["room"]);
+    room.push(msg["mapURL"]);
+    web.io.emit("servermessage", {
+        "room": msg["room"],
+        "command": "send_map",
+        "mapURL": msg["mapURL"]
+    });
+}
+
+//clear room data array
+function clear(msg)
+{
+    var room = getRoom(msg["room"]);
+    while (room.length) {
+		room.pop();
+	}
+    web.io.emit("servermessage", {
+        "room": msg["room"],
+        "command": "clear"
+    });
+}
+
+function join(msg)
+{
+    console.log("Client wants to join " + msg["room"]);
+    sendRoom(msg["room"]);
+}
+
+function handleMessage(msg)
+{
+    try
+    {
+        //check if message is valid
+        if (!("room" in msg &&
+            "command" in msg))
+            return;
+
+        if (msg["command"] == "draw") draw(msg);
+        if (msg["command"] == "join") join(msg);
+        if (msg["command"] == "clear") clear(msg);
+		if (msg["command"] == "map") map(msg);
+    }
+    catch (err)
+    {
+        console.log(err);
+    }
+}
+
+web.io.on('connection', function (socket) {
+    console.log((new Date()) + ' Connection established.');
+    socket.on("message", handleMessage);
+});
---
+++ server/web.js
@@ -0,0 +1,53 @@
+﻿//Configurations
+var PORT = 8443;
+var DEFAULT_PAGE = "/index.html";
+var HOST = 'localhost';
+var serverPort = 8443;
+
+//Setup
+var fs = require('fs');
+var express = require('express');
+var app = express();
+//var app = require('express')();
+var https = require('https');
+
+//var io = require('socket.io')(https);
+var key = fs.readFileSync('/etc/ssl/certs/mrbcleague_com.key');
+var cert = fs.readFileSync('/etc/ssl/certs/maps.mrbcleague.com.crt')
+var https_options = {
+    key: key,
+    cert: cert
+};
+app.use(express.static('client'));
+
+var options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm', 'html', 'js', 'css', 'png', 'jpg'],
+    index: DEFAULT_PAGE,
+    maxAge: '1d',
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+        res.set('x-timestamp', Date.now())
+    }
+};
+
+var server = https.createServer(https_options, app);
+var io = require('socket.io')(server);
+
+exports.start = function (port) {
+    PORT = port;
+    //Start listening
+    server.listen(PORT, '192.223.29.213', function() {
+  	console.log('server up and running at %s port', port);
+	});
+    //var server = https.listen(PORT, function () {
+        //var host = server.address().address
+        //var port = server.address().port
+        //console.log('Server listening at https://%s:%s', host, port)
+    //});
+
+    exports.app = app;
+    exports.server = server;
+    exports.io = io;
+};
