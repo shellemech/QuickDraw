50d2ae33a7854e6e9f70cb4816280781adce8f0a
Shellemech <shellemech@gmail.com>
2018-12-02 02:28:08 -0600

    input checks and updates

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,17 +1,12 @@
 ﻿$( document ).ready(function() {
-	$('#empty_msg').hide();
-	$('#btn_connect').click(function(){
-		if (!$('#txt_room').val()) {
-			$('#empty_msg').show(800);
+	$('#btn_connect').attr('disabled', 'true');
+	$('#txt_room').bind('input', function() {
+		if ($(this).val().length > 0) {
+			$("#btn_connect").removeAttr("disabled");
+			$('#empty_msg').hide(800);
+		} else {
+			$("#btn_connect").attr("disabled", "true");        
 		}
-		else btn_connect_pressed();
-	});
+	});    
 });
-
-function btn_connect_pressed() {
-	var txt_room = document.getElementById("txt_room");
-	var room_name = txt_room.value;
-	window.location.href = "/draw.html?room=" + room_name;
-}
-
 
--- server/web.js
+++ server/web.js
@@ -7,31 +7,45 @@ var HOST = 'localhost';
 var express = require('express');
 var app = express();
 var http = require('http');
-
+var helmet = require('helmet');
+var bodyParser = require("body-parser");
+var path = require("path");
+app.use(helmet());
 app.use(express.static('client'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 var options = {
-    dotfiles: 'ignore',
-    etag: false,
-    extensions: ['htm', 'html', 'js', 'css', 'png', 'jpg'],
-    index: DEFAULT_PAGE,
-    maxAge: '1d',
-    redirect: false,
-    setHeaders: function (res, path, stat) {
-        res.set('x-timestamp', Date.now())
-    }
+	dotfiles: 'ignore',
+	etag: false,
+	extensions: ['htm', 'html', 'js', 'css', 'png', 'jpg'],
+	index: DEFAULT_PAGE,
+	root: __dirname + '/client/',
+	maxAge: '1d',
+	redirect: false,
+	setHeaders: function (res, path, stat) {
+		res.set('x-timestamp', Date.now())
+	}
 };
 
 var server = http.createServer(app);
 var io = require('socket.io')(server);
 
+//Routes
+
+app.post('/', function (req, res) {
+	var room = req.body.txt_room;
+	var cleanroom = room.replace(/[|&;$%@"<>()+,]/g, "");
+        res.redirect('/draw.html?room=' + cleanroom);
+});
+
+//Start
 exports.start = function () {
-    //Start listening
-    server.listen(PORT, HOST, function() {
-  	console.log('server up and running at %s port', PORT);
+	server.listen(PORT, HOST, function() {
+		console.log('server up and running at %s port', PORT);
 	});
 
-    exports.app = app;
-    exports.server = server;
-    exports.io = io;
+	exports.app = app;
+	exports.server = server;
+	exports.io = io;
 };
